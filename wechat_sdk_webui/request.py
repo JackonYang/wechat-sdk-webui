217ca529889a40e896819c0bff57335686c68b7a
Jackon Yang <jackon.yang@seedlinktech.com>
2016-09-25 00:27:58 +0800

    save useful data into _db

--- wechat_sdk_webui/request.py
+++ wechat_sdk_webui/request.py
@@ -13,9 +13,14 @@ import utils
 # 微信根据 cookie 对用户鉴权
 _req = requests.Session()
 _db = {
+    'uuid': None,
     'pass_ticket': None,
     'base_request': None,
     'username': None,
+    'uin': None,
+    'sid': None,
+    'skey': None,
+    'sync_key': None,
 }
 
 
@@ -40,6 +45,7 @@ def get_uuid():
         if code != '200':
             print 'Unexpect Response Code in get_uuid: %s' % code
         else:
+            _db['uuid'] = uuid
             return uuid
     return None
 
@@ -51,7 +57,10 @@ def get_qrcode(uuid):
 
 
 def is_logined(uuid):
-    """detect whether user has logined"""
+    """detect whether user has logined
+
+    return redirect_uri
+    """
     url = 'https://login.wx.qq.com/cgi-bin/mmwebwx-bin/login'
     payload = {
         'uuid': uuid,
@@ -75,6 +84,7 @@ def did_login(redirect_uri):
 
     params = {n.nodeName: n.firstChild and n.firstChild.nodeValue
               for n in root.childNodes}
+
     _db['base_request'] = {
         'Uin': params['wxuin'],
         'Sid': params['wxsid'],
@@ -82,6 +92,9 @@ def did_login(redirect_uri):
         'DeviceID': utils.gen_device_id(),
     }
     _db['pass_ticket'] = params.get('pass_ticket')
+    _db['uin'] = params.get('wxuin')
+    _db['sid'] = params.get('wxsid')
+    _db['skey'] = params.get('skey')
 
 
 def wx_init():
@@ -94,6 +107,10 @@ def wx_init():
 
     _db['username'] = data.get('User', dict()).get('UserName')
     _db['skey'] = data.get('SKey')
+    _db['sync_key'] = data.get('SyncKey')
+
+    for key, value in _db.items():
+        print key, value
     return data
 
 
